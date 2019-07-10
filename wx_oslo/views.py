a70cf8664c789192beae63fcfedba7c64c41854c
Oslo <18248392627@163.com>
2019-07-10 11:40:32 +0800

    fis

--- wx_oslo/views.py
+++ wx_oslo/views.py
@@ -104,7 +104,9 @@ def get_tags(request):
     access_token = get_token()
     url = 'https://api.weixin.qq.com/cgi-bin/tags/get?access_token='+access_token
     result = urllib2.urlopen(url).read()
+    print result
     jso = json.loads(result).get('tag')
+    print jso
     for i in jso:
         if tag_name in i.get('name'):
             print '我在'
