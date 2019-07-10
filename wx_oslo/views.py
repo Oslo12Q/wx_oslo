a6e86a6cf860d613ccef0e9064498ddb8f48dddb
Oslo <18248392627@163.com>
2019-07-10 12:01:28 +0800

    fis

--- wx_oslo/views.py
+++ wx_oslo/views.py
@@ -54,6 +54,7 @@ def parseTxtMsg(request):
 
     if MsgType == 'text':
         Content = xml.find('Content').text
+        print Content
         dic = {u'平顶山',u'朝阳',u'海淀'}
         if Content in dic:
             ts = get_tags(Content)
